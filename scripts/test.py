4e17d22d83fdb41c979fe7aba75576e1a321cb4d
phantamanta44 <evandalong@gmail.com>
2019-04-24 18:58:25 -0500

    Transform joysticks to zero at neutral in test script

--- scripts/test.py
+++ scripts/test.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
         magn_x = controller.axis(codes.axis.ABS_X)
         magn_y = controller.axis(codes.axis.ABS_Y)
         spin('joy_magn', [
-            magn_x.join(magn_y, lambda x, y: (x**2 + y**2)**0.5).map(Float32).sink('joy_magn', Float32)
+            magn_x.join(magn_y, lambda x, y: ((x - 128)**2 + (y - 128)**2)**0.5).map(Float32).sink('joy_magn', Float32)
         ])
